1693d784a82d560193eeb4a4776240b8b9a76013
asgrig11 <asgrig11@mts.ru>
2018-04-28 09:03:57 +0300

    Weighted Uniform Strings solution

--- src/hackerRank/String/WeightedUniformStrings/Solution.java
+++ src/hackerRank/String/WeightedUniformStrings/Solution.java
@@ -1,24 +1,40 @@
 package hackerRank.String.WeightedUniformStrings;
 
-import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.HashSet;
 
 public class Solution {
 
-    private static String[] weightedUniformStrings(String s, int[] queries) {
-
-        Map<String, Long> frequentChars = Arrays.stream(
-                s.toLowerCase().split("")).collect(Collectors.groupingBy(c -> c, Collectors.counting()));
-
-
-
+    private static int getWeight(String s) {
+        return Character.getNumericValue(s.charAt(0)) - 9;
     }
 
-    public static void main(String[] args) throws IOException {
+
+    private static String[] weightedUniformStrings(String s, int[] queries) {
+
+        HashSet<Integer> weightSet = new HashSet<>();
+        int weight = 0;
+        char lastChar = ' ';
+
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            if (currentChar != lastChar) weight = 0;
+            weight += currentChar - 'a' + 1;
+            weightSet.add(weight);
+            lastChar = currentChar;
+        }
+
+        String[] listConsists = new String[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            listConsists[i] = weightSet.contains(queries[i]) ? "Yes" : "No";
+        }
+
+        return listConsists;
+    }
+
+    public static void main(String[] args) {
 
         String s = "abccddde";
-        int[] queries = new int[]{6, 1, 3, 12, 5, 9, 10};
+        int[] queries = new int[]{1, 3, 12, 5, 9, 10};
 
         String[] result = weightedUniformStrings(s, queries);
 
